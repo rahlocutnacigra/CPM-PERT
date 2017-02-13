2925328dc140393a00474456defb97f5dcad7038
Manca Cör <manca.cor@gmail.com>
2017-02-13 21:42:35 +0100

    Histogram + komentarji

--- CPM-PERT model/Monte Carlo.R
+++ CPM-PERT model/Monte Carlo.R
@@ -1,16 +1,27 @@
 source("CPM-PERT model/Program.R",encoding="UTF-8")
-A<-c()
 
-for (i in 1:10000){
+#Če je čas slučajna spremenljivka, porazdeljena enakomerno zvezno na intervalu [1,20], je 
+#pričakovana vrednost časa posameznega opravila enaka 10, varianca pa je enaka 100/3
+
+A<-c()
+for (i in 1:20000){
   C<-runif(18,0,20)
   A<-c(A,trajanje(Opr,Pred,C))
 }
-plot(A)
-mean(A)
-var(A)
+#S poskusom za 20000 ponovitev (precej dolgotrajen), sem za pričakovano vrednost dobila 96.854096,
+#za varianco pa 227.17752
+E<-mean(A)
+V<-var(A)
+hist(A)
+#Histogram, ki ga dobimo v tem primeru ima obliko normalne porazdelitve 
+
 B<-c()
-for (i in 1:2000){
+for (i in 1:500){
   D<-runif(18,0,20)
   B<-c(B,trajanje(Opr,Pred,D))
 }
-plot(B)
+
+E1<-mean(B)
+V1<-var(B)
+
+hist(B)
