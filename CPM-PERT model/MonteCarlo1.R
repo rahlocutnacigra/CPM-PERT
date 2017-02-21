4d0b462941e5305d11f067074d142107902ab8cb
Manca Cör <manca.cor@student.fmf.uni-lj.si>
2017-02-21 15:24:05 +0100

    Razčlenitev

---
+++ CPM-PERT model/MonteCarlo1.R
@@ -0,0 +1,20 @@
+#V naslednjem delu je čas opravljanja posamezne naloge porazdeljen okrnjeno normalno
+
+B <- sapply(1:1000, function(i) trajanje(Opr,Pred,rtnorm(18,50, 10, lower = 0, upper = 200)))
+hist(B)
+
+#Vidimo, da porazdelitev trajanja tudi v tem primeru spominja na normalno porazdelitev. Verjetno
+#bi z vedno večjim številom poskusov dobili vedno "bolj normalno" porazdelitev
+
+O2<-c()
+P2<-c()
+R2<-c()
+for(j in 1:100){
+  B1 <- sapply(1:1000, function(i) trajanje(Opr,Pred,rtnorm(18,50, j, lower = 0, upper = 200)))
+  P2<-c(P2,mean(B1))
+  R2<-c(R2, var(B1))
+  O2<-c(O2,j)
+}
+
+plot(O2,P2)#odvisnost med varianco trajanja posameznih opravil in varanco trajanja projekta
+plot(O2,R2)#odvisnost med obema variancama
---
+++ CPM-PERT model/MonteCarlo2.R
@@ -0,0 +1,33 @@
+#Eksponentna porazdelitev posameznih opravil
+C <- sapply(1:1000, function(i) trajanje(Opr,Pred,rexp(18, rate =1)))
+hist(C)
+#pri eksponentni porazdelitvi trajanja opravil spet ugotovimo, da je porazdelitev slučajne 
+#spremenljivke trajanja projekta podobna normalni
+
+#najprej izvedemo simulacijo za lambda < 1
+O1<-c()
+P1<-c()
+R1<-c()
+for(j in seq(0.1,1,0.01)){
+  C1 <- sapply(1:100, function(i) trajanje(Opr,Pred,rexp(18, rate =j)))
+  P1<-c(P1,mean(C1))
+  R1<-c(R1, var(C1))
+  O1<-c(O1,j)
+}
+plot(1/O1,P1)#odvisnost pričakovane vrednosti trajanja projekta od pričakovanega trajanja opravil
+plot(1/O1^2,R1)#odvisnost varianc pri eksponentni porazdelitvi
+plot(1/O1^2,P1)#odvisnost pričakovanega trajanja projekta od variance trajanja posameznih opravil
+
+#za lambda > 1
+O3<-c()
+P3<-c()
+R3<-c()
+for(j in seq(1,10,0.1)){
+  C2 <- sapply(1:100, function(i) trajanje(Opr,Pred,rexp(18, rate =j)))
+  P3<-c(P3,mean(C2))
+  R3<-c(R3, var(C2))
+  O3<-c(O3,j)
+}
+plot(1/O3,P3)#odvisnost pričakovane vrednosti trajanja projekta od pričakovane vrednosti trajanja opravil
+plot(1/O3^2,P3)#odvisnost pričakovane vrednosti trajanja projekta od variance trajanja posameunih opravil
+plot(1/O3^2,R3)#odvisnost varianc
